0873e86da8d0d33682ba65cdf3a52d49a62866a1
Haowei <hzhao6@wpi.edu>
2018-11-26 21:44:11 -0500

    Final push

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
 
+# Read data path from the csv file
 def readData():
     samples = []
 
@@ -20,6 +21,8 @@ def readData():
 
     return samples
 
+
+# Generator is used so data would not eat up the memory when training the network. 
 def generator(samples, batch_size=32, correction = 0.2):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
@@ -35,9 +38,11 @@ def generator(samples, batch_size=32, correction = 0.2):
                 center_angle = float(batch_sample[3])
                 assert center_angle is not None, 'Center angle is None!!'
                 assert center_image is not None, 'Center image is None!!'
-                center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
+                # Simulation use RGB image as input.
+                center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB) 
                 images.append(center_image)
                 angles.append(center_angle)
+                # Images are filped to produce more data
                 images.append(cv2.flip(center_image,1))
                 angles.append(center_angle*-1.0)
 
@@ -46,9 +51,12 @@ def generator(samples, batch_size=32, correction = 0.2):
                 left_angle = float(batch_sample[3])
                 assert left_angle is not None, 'Left angle is None'
                 assert left_image is not None, 'Left image is None'
+                # Simulation use RGB image as input.
                 left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB)
                 images.append(left_image)
+                # Apply a correction term so the car can recover from diverging. 
                 angles.append(left_angle + correction)
+                # Images are filped to produce more data
                 images.append(cv2.flip(left_image,1))
                 angles.append((left_angle+correction)*-1.0)
 
@@ -57,9 +65,12 @@ def generator(samples, batch_size=32, correction = 0.2):
                 right_angle = float(batch_sample[3])
                 assert right_angle is not None, 'Right angle is None'
                 assert right_image is not None, 'Right image is None'
+                # Simulation use RGB image as input.
                 right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2RGB)
                 images.append(right_image)
+                # Apply a correction term so the car can recover from diverging. 
                 angles.append(right_angle - correction)
+                # Images are filped to produce more data
                 images.append(cv2.flip(right_image,1))
                 angles.append((right_angle-correction)*-1.0)
 
@@ -72,8 +83,11 @@ def generator(samples, batch_size=32, correction = 0.2):
 
 def nVaidiaModel():
     model = Sequential()
+    # Normalized the images
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+    # Only keep the parts that capture the road. 
     model.add(Cropping2D(cropping=((50, 20), (0, 0))))
+    # The NVIDIA model
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
